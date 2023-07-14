a33ce8f9724c6a1a3b17c7be6f1f45614a820d87
dina2803 <dina@MacBook-Air-Dina.local>
2023-07-14 22:49:55 +0600

    dz

--- app.js
+++ app.js
@@ -21,3 +21,26 @@ switch (trafficLight) {
     default:
         console.log('does not work')
 }
+
+var names = ['Bishkek', 'Los Angeles', 'Chicago', 'New York', 'Houston', 'Dallas', 'Philadelphia', 'Seattle']
+for (let i = 6; i < names.length; i++) {
+    console.log(names)}
+for (let i = 0; i < names.length; i++) {
+    if(names[i][0] === 's' || names[i][0] === 'S'||
+        names[i][1] === 's' || names[i][10] === 'S'||
+        names[i][2] === 's' ||
+        names[i][3] === 's' ||
+        names[i][4] === 's' ||
+        names[i][5] === 's' ||
+        names[i][6] === 's' ||
+        names[i][7] === 's' ||
+        names[i][8] === 's' ||
+        names[i][9] === 's' ||
+        names[i][10] === 's'||
+        names[i][11] === 's'){
+        console.log(`names with letter s - ${names[i]}`)
+    }
+    else{
+        console.log(`other names - ${names[i]}`)
+    }
+}
